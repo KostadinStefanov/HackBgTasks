742deec0a30400fd43f6ca5c43e862750aadf3ba
KostadinStefanov <kosstefanov@gmail.com>
2015-02-11 18:15:36 +0200

    added second task with 2 classes

---
+++ SimpleLogger/SimpleLogger/LoggerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    class LoggerTest
+    {
+        public static void Main()
+        {
+            var consoleLogger = new ConsoleLogger();
+            consoleLogger.Log(2, "Hack BG Warning Test");
+            
+            var fileLogger = new FileLogger();
+            fileLogger.Log(1, "Hack BG Info Test");
+            
+            
+        }
+    }
+}
